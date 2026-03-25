de54bc669a7e35e999badcadb294d3fa085d334e
deneth-rajapaksha <deneth.private@gmail.com>
2026-03-26 03:27:20 +0530

    config netlify deployment

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
-import { createRequestHandler } from '@netlify/angular-runtime';
-import bootstrap from './main.server';
+import { AngularAppEngine, createRequestHandler } from '@angular/ssr';
+import { getContext } from '@netlify/angular-runtime';
 
-export const handler = createRequestHandler({
-  bootstrap,
+const angularAppEngine = new AngularAppEngine();
+
+export default createRequestHandler(async (request) => {
+  const context = getContext();
+
+  const result = await angularAppEngine.handle(request, context);
+  return result || new Response('Not found', { status: 404 });
 });

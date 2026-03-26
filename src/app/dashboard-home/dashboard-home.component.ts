a7abaf4a267fc73cd617c6e2e8f1e57080e16e05
deneth-rajapaksha <deneth.private@gmail.com>
2026-03-26 15:32:36 +0530

    changed landing

--- src/app/dashboard-home/dashboard-home.component.ts
+++ src/app/dashboard-home/dashboard-home.component.ts
@@ -11,7 +11,7 @@ import { ShortcutCardsComponent } from '../shortcut-cards';
     <!-- Top Section: Simulator & Feed -->
     <div class="flex flex-col md:flex-row gap-6 md:gap-8 h-auto md:h-[500px]">
       <!-- Simulator takes priority in center/left -->
-      <div class="w-full md:flex-1 h-[400px] md:h-full order-1">
+      <div class="w-full md:flex-1 h-[550px] md:h-full order-1">
         <app-particle-simulator></app-particle-simulator>
       </div>
       
